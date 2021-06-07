43c55f6bce8e41932d4b3e39e2f688a3e65400ed
humberto <humbertorivero9@gmail.com>
2021-06-07 20:57:17 -0300

    mostrando el resultado

--- cotizador-seguros/src/components/Resumen.jsx
+++ cotizador-seguros/src/components/Resumen.jsx
@@ -1,4 +1,16 @@
-import React, { Fragment } from 'react'
+import React  from 'react';
+import styled from '@emotion/styled';
+import { primeraMayuscula } from '../helper'
+
+const ContenedorResumen = styled.div`
+    padding: 1rem;
+    text-align: center;
+    background-color: #00838F;
+    color: #fff;
+    margin-top: 1rem;
+`;
+
+
 
 function Resumen({datos}) {
 
@@ -7,15 +19,15 @@ function Resumen({datos}) {
     if(marca === '' || year === '' || plan === '') return null
 
     return (
-        <Fragment>
+        <ContenedorResumen>
             <h2>Resumen de Cotizacion</h2>
             <ul>
-                <li>Marca: </li>
-                <li>Año del Auto: </li>
-                <li>Plan: </li>
+                <li>Marca: {primeraMayuscula(marca)}</li>
+                <li>Año del Auto: {year}</li>
+                <li>Plan: {primeraMayuscula(plan)}</li>
             </ul>
 
-        </Fragment>
+        </ContenedorResumen>
     )
 }
 
